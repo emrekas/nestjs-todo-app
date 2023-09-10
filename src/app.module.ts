bf1637337e729889cce7b7cb88ae0c13a1e9bdd9
Emre KAŞ <emre.kas@akinon.com>
2023-09-11 01:05:24 +0200

    feat: added todo module defination into the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     UserModule,
+    TodoModule,
     PrismaModule,
   ],
 })
